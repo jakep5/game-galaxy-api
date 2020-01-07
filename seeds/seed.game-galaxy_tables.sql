a97658bc434bd691c0952d22c0d9099b824d1da1
jakep5 <jakepagel1@yahoo.com>
2020-01-07 16:50:30 -0600

    fix bugs in migration files

--- seeds/seed.game-galaxy_tables.sql
+++ seeds/seed.game-galaxy_tables.sql
@@ -1,5 +1,23 @@
-INSERT INTO 'gamegalaxy_folders'(id, name)
+BEGIN;
+
+TRUNCATE
+    'gamegalaxy_folders',
+    'gamegalaxy_games',
+    'gamegalaxy_users'
+    RESTART IDENTITY CASCADE;
+
+INSERT INTO 'gamegalaxy_users' (id, user_name, password)
+VALUES
+    (1, 'testuser', 'testpassword');
+
+INSERT INTO 'gamegalaxy_folders' (id, name)
 VALUES
     (1, 'Action'),
     (2, 'Adventure'),
     (3, 'Fantasy');
+
+INSERT INTO 'gamegalaxy_games' (title, completed)
+VALUES
+    ('The Legend of Zelda: Ocarina of Time', false),
+    ('Super Mario Odyssey', false),
+    ('Borderlands 3', false)
