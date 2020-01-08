fe27c403c2c67a42bbd4d9e4d16107e8532e14c0
jakep5 <jakepagel1@yahoo.com>
2020-01-08 13:07:57 -0600

    finalize auth router and service

---
+++ src/auth/auth-router.js
@@ -0,0 +1,49 @@
+const express = require('express')
+const AuthService = require('./auth-service')
+
+const authRouter = express.Router();
+
+const jsonBodyParser = express.json();
+
+authRouter
+    .post('/login', jsonBodyParser, (req, res, next) => {
+        const {user_name, password} = req.body;
+
+        const currentLoginUser = {user_name, password}
+
+        for (const [key, value] of Object.entries(currentLoginUser))
+            if (value == null) {
+                return res.status(400).json({
+                    error: `Request body is missing ${key}.`
+                })
+            }
+
+            AuthService.getUserWithUserName(
+                req.app.get('db'),
+                currentLoginUser.user_name
+            )
+                .then(user => {
+                    if(!user) {
+                        return res.status(400).json({
+                            error: 'Incorrect username and/or password',
+                        })
+                    }
+
+                    return AuthService.comparePassword(currentLoginUser.password, user.password)
+                        .then(comparedPair => {
+                            if(!comparedPair)
+                                return res.status(400).json({
+                                    error: 'Incorrect username and/or password',
+                                })
+
+                                const sub = user.user_name;
+                                const payload = { user_id: user.id }
+                                res.send({
+                                    authToken: AuthService.makeJwt(sub, payload)
+                                })
+                        })
+                })
+                .catch(next)
+    })
+
+module.exports = authRouter;
--- src/config.js
+++ src/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
+    DATABASE_URL: process.env.DATABASE_URL || 'postgresql://postgres@localhost/game-galaxy',
+    TEST_DATABASE_URL: process.env.TEST_DATABASE_URL || 'postgresql://postgres@localhost/game-galaxy-test',
+    JWT_EXPIRY: process.env.JWT_EXPIRY || '360s',
+    JWT_SECRET: process.env.JWT_SECRET || 'change-this-secret',
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000'
 }
